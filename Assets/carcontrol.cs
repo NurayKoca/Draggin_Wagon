3787a02e83039c3628810c23a886257f40d5dda0
Nuray KOCA <nuraykoca0738@gmail.com>
2022-03-14 15:53:39 +0300

    İlerleme5-Week04

--- Assets/carcontrol.cs
+++ Assets/carcontrol.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 using System;
 public enum Axel{
 
-    front,
-    rear
+    Front,
+    Rear
 }
 [Serializable]
-public struct whell{
+public struct Whell{
 
-   public GameObject wheel;
-   public WheelCollider collider;
+   public GameObject model;
+   public   WheelCollider collider;
    public Axel axel; 
+   
+   
 }
 
 public class carcontrol : MonoBehaviour
@@ -20,11 +22,11 @@ public class carcontrol : MonoBehaviour
     [SerializeField]
     private float MaxHizlanma = 200f;
     [SerializeField]
-    private float dönüşHassasiyeti = 1f;
+    private float donusHassasiyeti = 1f;
     [SerializeField]
     private float MaximumDonusAcisi = 45f;
     [SerializeField]
-    private List<whell> wheels;
+    private List<Whell> wheels;
     private float inputX, inputY;
     
     
@@ -38,11 +40,15 @@ public class carcontrol : MonoBehaviour
     private void LateUpdate()
     {
        Move();
+       Turn();
+       FrenYap();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+          Tekerlerindonusu();
           HareketYonu();
     }
 
@@ -60,5 +66,53 @@ public class carcontrol : MonoBehaviour
        }
 
     }
-   
+   void Turn()
+   {
+
+    foreach(var wheel in wheels)
+    {
+
+        if(wheel.axel == Axel.Front)
+        {
+ 
+            var _steerAngle = inputX * donusHassasiyeti * MaximumDonusAcisi;
+
+            wheel.collider.steerAngle = Mathf.Lerp(wheel.collider.steerAngle, _steerAngle, .1f);
+        }
+    }
+
+   }
+
+   void Tekerlerindonusu()
+   {
+
+       foreach (var wheel in wheels)
+       {
+
+           Quaternion _rot;
+           Vector3 _pos;
+           wheel.collider.GetWorldPose(out _pos, out _rot);           
+         //wheel.model.transform.position = _pos;
+         //wheel.model.transform.rotation = _rot;
+
+       }
+   }
+   void FrenYap(){
+
+       if(Input.GetKeyDown(KeyCode.Space)){
+
+           foreach(var wheel in wheels){
+
+               wheel.collider.brakeTorque = 10000;
+           }
+       }
+
+       if(Input.GetKeyUp(KeyCode.Space)){
+
+            foreach(var wheel in wheels){
+
+                wheel.collider.brakeTorque = 0;
+            }
+       }
+   }
 }
---
+++ week4.md
@@ -0,0 +1,4 @@
+"I added the suspension system of the race car and wheelcollider."
+"The race car started to move left and right."
+"The race car started to stop when he pressed the space key. When the button was released, it started to move again."
+ 
